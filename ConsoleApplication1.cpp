23a99f1f741316ce50d2be83e8692a5941fafddf
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-08 21:05:18 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,14 +4,33 @@
 using namespace std;
 int main()
 {
-	int x, ll,a,b;
-	cin >> x;
-	string y;
-
-	cin >> y;
-	for (a = 1; a < x+1; a++) {
-		cout << y << " ";
+	int x, ll,a,b,c;
+	cin >> a>>b>>c;
+	if (a > b) {
+		if (a > c) { 
+			cout << a << " ";
+			if (b > c) { cout << c; }
+			else { cout << b; }
+		}
+		else {
+			cout << c<<" ";
+			cout << b;
+		}
 	}
+	else {
+		if (b > c) {
+			cout << b << " "; 
+			if (a > c) {
+				cout << c;
+			}
+			else { cout << a; }
+		}
+		else {
+			cout << c << " ";
+			cout << a;
+		}
+	}
+	
 
 cin >> ll;
 return 0;
