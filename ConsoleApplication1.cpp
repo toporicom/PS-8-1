9d4269191903ff1cffbc958ffd469e7352888652
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-08 19:57:04 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 int main()
 {
-	int x, y;
-cout << "Farhad";
-cin >> x;
+	int x, y,ll;
+	cin >> x;
+	y = x * x * x * x * x + x * x * x * x + x * x * x + x * x + 25;
+
+	cout << y;
+
+cin >> ll;
 return 0;
 
 }
