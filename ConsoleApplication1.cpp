26bbccc05bf0963c4a934f2c90278720b2f2c643
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-08 21:25:08 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,32 +4,92 @@
 using namespace std;
 int main()
 {
-	int x, ll,a,b,c;
-	cin >> a>>b>>c;
+	int x, ll,a,b,c,d;
+	cin >> a>>b>>c>>d;
 	if (a > b) {
-		if (a > c) { 
-			cout << a << " ";
-			if (b > c) { cout << c; }
-			else { cout << b; }
+		if (a > c) {
+			if (a > d) {
+				cout << a << " ";
+			}
+			else {
+				cout << d << " ";
+			}
+
 		}
 		else {
-			cout << c<<" ";
-			cout << b;
+			if (c > d) {
+				cout << c << " ";
+			}
+			else {
+				cout << d << " ";
+			}
 		}
 	}
 	else {
 		if (b > c) {
-			cout << b << " "; 
-			if (a > c) {
-				cout << c;
+			if (b > d) {
+				cout << b << " ";
+
 			}
-			else { cout << a; }
+			else {
+				cout << d << " ";
+
+			}
 		}
 		else {
-			cout << c << " ";
-			cout << a;
+			if (c > d) {
+				cout << c << " ";
+
+			}
+			else {
+				cout << d << " ";
+
+			}
 		}
 	}
+	if (a < b) {
+		if (a < c) {
+			if (a < d) {
+				cout << a << " ";
+			}
+			else {
+				cout << d << " ";
+			}
+
+		}
+		else {
+			if (c < d) {
+				cout << c << " ";
+			}
+			else {
+				cout << d << " ";
+			}
+		}
+	}
+	else {
+		if (b < c) {
+			if (b < d) {
+				cout << b << " ";
+
+			}
+			else {
+				cout << d << " ";
+
+			}
+		}
+		else {
+			if (c < d) {
+				cout << c << " ";
+
+			}
+			else {
+				cout << d << " ";
+
+			}
+		}
+	}
+		
+	
 	
 
 cin >> ll;
