0995ce0adf2225ea0a8796caa82982162d6ae282
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-15 21:19:46 +0300

    Add files via upload

---
+++ задание 1.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int  n,i,a;
+	cin >> n;
+	cout << "write your name: ";
+
+	 string name;
+	 cin >> name;
+	 if (n % 2 == 0) {
+		 for (i = 1; i <= n; i++) {
+			 cout << name<<" ";
+		 }
+
+	 }
+	 else {
+		 cout << "C++ is very cool";
+	 }
+
+
+	 return 0;
+	
+	 cin >> a;
+
+  
+}
+
