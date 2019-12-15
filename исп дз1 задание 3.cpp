66dd90c19b27170b678389325870f42ec6a96e5a
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-15 23:14:09 +0300

    Add files via upload

---
+++ исп дз1 задание 3.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	int x, y, ll, a, b,i;
+	cin >> x;
+	for (i = x - 1; i >= 1; i--) {
+		cout << i<<" ";
+	}
+	cin >> ll;
+	return 0;
+
+}
