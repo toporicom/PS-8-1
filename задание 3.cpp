ee223d0ba6d6368291911698168e88c5384185b3
Фархад <58259681+toporicom@users.noreply.github.com>
2019-12-15 22:32:50 +0300

    Add files via upload

---
+++ задание 3.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int  n, i, lll, b, x, y, k, j,p;
+
+	cin >> n;
+	vector<int> a(n);
+	
+	for (i = 0; i < n; i++) {
+		cin >> a[i];
+	
+	}
+	
+	cin >> p;
+	
+	if (a[p-1] % 9 == 1) {
+		cout << "find it";
+
+	}
+	else {
+		cout << a[p-1];
+	}
+
+
+
+	cin >> lll;
+
+	return 0;
+}
+
+
