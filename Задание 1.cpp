432200e2d0fbccdd484bfcb3aef182e74bff3dc3
Фархад <58259681+toporicom@users.noreply.github.com>
2020-02-01 13:07:39 +0300

    Add files via upload

---
+++ Задание 1.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	int a, b, c, d, word,i;
+	cin >> a;
+	cin >> b;
+	for ( i = 0; i<b; i++) {
+		if (a % 10 == 0) {
+			a = a / 10;
+		}
+		else {
+			a = a - 1;
+		}
+    }
+	cout << a;
+
+
+
+	return 0;
+
+
+}
