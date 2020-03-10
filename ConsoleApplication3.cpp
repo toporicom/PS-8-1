858ee1f985eca73bfb6fd6b9dac1c9495bf65235
Фархад <58259681+toporicom@users.noreply.github.com>
2020-03-10 13:18:48 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	int i, j, n;
+	long long x, y;
+	cin >> n;
+	y = 0;
+	vector<int> a(n);
+	for (i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (i - j >= 8 or i - j <= -8) {
+				x = a[i] * a[j];
+				if (x > y) {
+					y = x;
+				}
+			}
+		}
+	}
+	cout << y;
+	return 0;
+}
